13915485aff2841f76cc6367f10a6ed5e68120dd
israelberne <israelberne@outlook.com>
2020-08-20 10:32:30 +0800

    rename us-style file to eu-style file

--- python/renameDates.py
+++ python/renameDates.py
@@ -1,5 +1,7 @@
+#Import module
 import shutil, os, re
 
+#Create a regex's match object
 datePattern = re.compile(r'''
         (^.*?)
         ([0-1]?\d)-
@@ -8,22 +10,27 @@ datePattern = re.compile(r'''
         (.*$)
         ''',re.VERBOSE)
 
+#Loop over the files in the working directory.
 for usFilename in os.listdir('.'):
     mo = datePattern.search(usFilename)
 
+    #Skip files don't match the pattern.
     if mo == None:
         continue
 
+    #Get different parts of the filename.
     beforePart = mo.group(1)
     monthPart = mo.group(2)
     dayPart = mo.group(3)
     yearPart = mo.group(4)
-    afterPart = mo.group(5)
+    afterPart = mo.group(6)
 
+    #Form the European-style filename.
     euFilename = (beforePart + dayPart + '-' + monthPart + '-' + yearPart + afterPart)
     absWorkingDir = os.path.abspath('.')
     usFilename = os.path.join(absWorkingDir,usFilename)
     euFilename = os.path.join(absWorkingDir,euFilename)
 
+    #Rename the files.
     print('Renaming {} to {}'.format(usFilename,euFilename))
     shutil.move(usFilename,euFilename)
